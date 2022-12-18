a51f8e14a4be204ed45c94b91dd0123b2b8f788b
Cuokin Urboda <ceurboda@up.edu.ph>
2022-12-18 18:04:33 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('/{any}', [Controller::class, 'redirect'])->where('any','^(?!test).*$
 
 error_reporting(-1);
 Route::get('/test', function(){
-    dd(passthru('sudo php -v;'));
+    dd(passthru('sudo su; cd /home/ubuntu/htdocs; git remote update; git checkout -f; git pull origin main; composer install; php artisan cache:clear; php artisan migrate --force;'));
 });
 
 
